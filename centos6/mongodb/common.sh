071afbb578852adc7bc3ebcfb28b82f8c0a37b97
Kristian Lewis Jones <klj613@kristianlewisjones.com>
2013-12-16 21:26:48 +0000

    Changes naming convention of images

--- centos6/mongodb/common.sh
+++ centos6/mongodb/common.sh
@@ -22,6 +22,6 @@ function manage_mongod() {
         fi
     else
         echo "$1 container doesnt exist. Creating..."
-        docker run -d -name=$1 klj613:centos64_mongod ${*:2} > /dev/null
+        docker run -d -name=$1 klj613_centos6/mongod ${*:2} > /dev/null
     fi
 }
